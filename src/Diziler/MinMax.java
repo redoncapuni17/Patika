6feeff8bc5b0d7e7f795c38c810622e7309af240
Redon Capuni <redoncapuni@gmail.com>
2022-08-29 00:11:37 +0300

    Java

---
+++ src/Diziler/MinMax.java
@@ -0,0 +1,31 @@
+package Diziler;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinMax {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int[] list = {15,12,788,1,-1,-778,2,0};
+        int min = list[0];
+        int max = list[0];
+
+        System.out.println(Arrays.toString(list));
+        Arrays.sort(list);
+        System.out.println(Arrays.toString(list));
+        System.out.print("Girilen Sayi : ");
+        int number = input.nextInt();
+
+        for(int i:list){
+            if(i<number){
+                min=i;
+            }
+            if(i>number){
+                max=i;
+                break;
+            }
+        }
+        System.out.println("Girilen sayıdan küçük en yakın sayı : "+min);
+        System.out.println("Girilen sayıdan büyük en yakın sayı : "+max);
+    }
+}
