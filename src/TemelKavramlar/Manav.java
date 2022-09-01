39974f6fed228cf051aa43eeafdf2890bf94554c
Redon Capuni <redoncapuni@gmail.com>
2022-09-01 11:02:16 +0300

    Java

--- src/TemelKavramlar/Manav.java
+++ src/TemelKavramlar/Manav.java
@@ -5,15 +5,27 @@ import java.util.Scanner;
 public class Manav {
     public static void main(String[] args) {
 
-        int points=6;
-        switch(points)
-        {
-            case 6: ;
-            case 7: System.out.println("Java");break;
-            case 8: ;
-            case 9: System.out.println("101");break;
-            case 10: System.out.println("Patika"); break;
-            default: System.out.println("Dev");
-        }
+        Scanner input = new Scanner(System.in);
+        double armut =2.14;
+        double elma = 3.67;
+        double domates = 1.11;
+        double muz = 0.95;
+        double patlican =5.00;
+
+        System.out.print("Armut Kaç Kilo : ");
+        int armutKilo = input.nextInt();
+        System.out.print("Elma Kaç Kilo : ");
+        int elmaKilo = input.nextInt();
+        System.out.print("Domates Kaç Kilo : ");
+        int domatesKilo = input.nextInt();
+        System.out.print("Muz Kaç Kilo : ");
+        int muzKilo = input.nextInt();
+        System.out.print("Patlıcan Kaç Kilo : ");
+        int patlicanKilo = input.nextInt();
+        double toplam = ((armut*armutKilo)+(elma*elmaKilo)+(domates*domatesKilo)+(muz*muzKilo)+(patlican*patlicanKilo));
+        System.out.println("Toplam Tutar : "+toplam+" TL");
+
+
+
     }
 }
