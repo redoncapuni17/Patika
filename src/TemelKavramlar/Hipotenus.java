d5e8e141c3c27582335acc3c153a1513da84b983
Redon Capuni <redoncapuni@gmail.com>
2022-09-03 23:06:51 +0300

    Java

--- src/TemelKavramlar/Hipotenus.java
+++ src/TemelKavramlar/Hipotenus.java
@@ -5,19 +5,10 @@ import java.io.*;
 
 class Main {
 
-    public static int FirstFactorial(int num) {
-        int fact = 1;
-        while(num>0){
-            fact=fact*num;
-            num--;
-        }
-        return num;
-    }
-
     public static void main (String[] args) {
-        // keep this function call here
-        Scanner s = new Scanner(System.in);
-        System.out.print(FirstFactorial(s.nextInt()));
+        if(args.length>0){
+            System.out.println(args.length);
+        }
     }
 
 }
