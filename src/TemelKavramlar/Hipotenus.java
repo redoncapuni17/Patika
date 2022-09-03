4ecc8f206395927ec21f8fa540607929b039ed8f
Redon Capuni <redoncapuni@gmail.com>
2022-09-03 23:45:50 +0300

    Java

--- src/TemelKavramlar/Hipotenus.java
+++ src/TemelKavramlar/Hipotenus.java
@@ -7,8 +7,8 @@ class Main {
 
     public static void main (String[] args) {
         if(args.length>0){
-            System.out.println(args.length);
+            System.out.println(args.length;
+
         }
     }
-
 }
